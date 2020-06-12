aa49d46a466e2baaed3bec90c432f4e600e4a725
parkhomchukp <55126390+parkhomchukp@users.noreply.github.com>
2020-06-12 11:45:47 +0300

    Update mario_harder.c

--- mario_harder.c
+++ mario_harder.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int get_positive_int(string prompt);
+
+int main(void)
+{
+    int height;
+    //getting value of hihgt
+    height = get_positive_int("Height: ");
+    //main cycle for new lines
+    for (int enters = 0; enters < height; enters++)
+    {
+        //cycle for typing spaces behind hashes
+        for (int spaces = height - 1; spaces > enters; spaces--)
+        {
+            printf(" ");
+        }
+        //cycle for typing left part of hashes
+        for (int hashes = -1; hashes < enters; hashes++)
+        {
+            printf("#");
+        }
+        //typing 2 spaces between hashes
+        printf("  ");
+        //cecle for typing right part of hashes
+        for( int hashes2 = -1; hashes2 < enters; hashes2++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+//function for getting only positive integer from 1 to 8 from user
+int get_positive_int(string prompt)
+{
+    int n;
+    do
+    {
+        n = get_int("%s", prompt);
+    }
+    while (n < 1 || n > 8);
+    return n;
+}
